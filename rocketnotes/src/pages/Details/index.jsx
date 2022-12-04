9f8fb4aae4bfb64011ca7b716ec8b364b4bf6196
Ícaro Felizardo <60118701+Icaro-Felizardo@users.noreply.github.com>
2022-12-04 13:45:52 -0300

    Instalação do vite e estrutura básica do projeto

---
+++ rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import "./styles.js"
+import { Container } from "./styles.js"
+import { Button } from "../../components/button"
+
+export function Details(){
+
+  return(
+    <Container>
+      <h1>Hello World</h1>
+      <Button title="Teste" />
+    </Container>
+  )
+}
