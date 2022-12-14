f1d64a2c378c41235fbffb447ac1e0f4349217f5
Ícaro Felizardo <60118701+Icaro-Felizardo@users.noreply.github.com>
2022-12-13 22:51:49 -0300

    Criação de telas de login logout e perfil do usuário

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -4,13 +4,15 @@ import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
 import { SignIn } from './pages/Signin'
+import { SignUp } from './pages/SignUp'
+import { Profile } from './pages/Profile'
 import { Home } from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={ theme }>
       <GlobalStyles />
-      <SignIn />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 )
--- rocketnotes/src/pages/SignUp/index.jsx
+++ rocketnotes/src/pages/SignUp/index.jsx
@@ -1,17 +1,26 @@
 import { Container, Form, Background } from "./styles";
-import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+import { FiLogIn, FiMail, FiLock, FiUser } from "react-icons/fi";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
-export function SignIn(){
+export function SignUp(){
     return (
         <Container>
+
+            <Background />
+
             <Form>
                 <h1>Rocket Notes</h1>
                 <p>Aplicação para salvar e gerenciar seus links</p>
 
-                <h2>Faça seu Login</h2>
+                <h2>Crie sua conta</h2>
+
+                <Input 
+                placeholder = "Nome"
+                type = "Text"
+                icon = {FiUser}
+                />
 
                 <Input 
                 placeholder = "Email"
@@ -24,12 +33,13 @@ export function SignIn(){
                 icon = {FiLock}
                 />
 
-                <Button title={"Entrar"} />
-                <a href="#">Criar Conta</a>                    
+                <Button title={"Cadastrar"} />
+                
+                <a href="#">Voltar para o login</a>                    
                 
             </Form>
 
-            <Background />
+            
         </Container>
     )
 }
