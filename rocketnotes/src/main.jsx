7c084407ba20de9461608da07357a8028eab722b
Ícaro Felizardo <60118701+Icaro-Felizardo@users.noreply.github.com>
2022-12-15 22:03:04 -0300

    Começo da tela de criação de notas

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -7,12 +7,13 @@ import { SignIn } from './pages/Signin'
 import { SignUp } from './pages/SignUp'
 import { Profile } from './pages/Profile'
 import { Home } from './pages/Home'
+import { New } from './pages/New'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={ theme }>
       <GlobalStyles />
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 )
