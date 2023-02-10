92844d74a0555d3c0e7d3bf74818bd52108f2bc8
Ícaro Felizardo <60118701+Icaro-Felizardo@users.noreply.github.com>
2023-02-09 21:54:59 -0300

    SignIn

--- rocketnotes/src/pages/Signin/index.jsx
+++ rocketnotes/src/pages/Signin/index.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import { Container, Form, Background } from "./styles";
 import { FiMail, FiLock } from "react-icons/fi";
 import { Link } from 'react-router-dom'
+
+import {useAuth} from "../../hooks/auth"
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
 export function SignIn(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const {signIn} = useAuth();
+   
+    function handleSignIn(){
+        signIn({ email, password });
+    }
+
     return (
         <Container>
             <Form>
@@ -18,14 +30,17 @@ export function SignIn(){
                 placeholder = "Email"
                 type = "Text"
                 icon = {FiMail}
+                onChange = {e => setEmail(e.target.value) }
                 />
                 <Input 
                 placeholder = "Senha"
                 type = "password"
                 icon = {FiLock}
+                onChange = {e => setPassword(e.target.value) }
+
                 />
 
-                <Button title={"Entrar"} />
+                <Button title="Entrar" onPress={handleSignIn} />
                 <Link to="/criar-conta">Criar Conta</Link>                    
                 
             </Form>
